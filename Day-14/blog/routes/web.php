6e5e9ff14ff29219c1f167a0e4e7721be9bde0a8
JoneCOder <mohummadshorifulislam@gmail.com>
2019-11-24 16:08:57 +0600

    Laravel-vue-day-14 in 24/11/19, route mastering, install node.js, vue, Auth

---
+++ Day-14/blog/routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [
+    'uses' =>   'FrontendController@index',
+    'as' => 'index'
+]);
+
+Route::get('/about', [
+    'uses' => 'FrontendController@about',
+    'as' => 'about'
+]);
+
+Route::get('/products', [
+    'uses' => 'ProductController@product',
+    'as' => 'product'
+]);
+
+Route::get('/store', [
+    'uses' => 'StoreController@store',
+    'as' => 'store'
+]);
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
